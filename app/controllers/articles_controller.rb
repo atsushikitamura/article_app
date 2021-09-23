56339669f65f2a602fa4bc7e9cf36e2e6a0ac17e
atsushikitamura <ax2kx8@gmail.com>
2021-09-23 20:12:44 +0900

    投稿機能実装

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,2 +1,29 @@
 class ArticlesController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :create]
+  before_action :not_admin, only: [:new, :create]
+
+  def index
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(article_params)
+    if @article.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+  def not_admin
+    redirect_to root_path unless current_user.admin?
+  end
+
+  def article_params
+    params.require(:article).permit(:image, :title, :content).merge(user_id: current_user.id)
+  end
 end
