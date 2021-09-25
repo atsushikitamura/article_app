c60f7b2a359c81ea13467cf408e2676b0bdd5a45
atsushikitamura <ax2kx8@gmail.com>
2021-09-25 19:49:30 +0900

    Commentモデルテスト

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @comment = FactoryBot.build(:comment)
+  end
+
+  describe 'コメント投稿' do
+    context 'コメント投稿できるとき' do
+      it 'textが存在すれば登録できる' do
+        expect(@comment).to be_valid
+      end
+    end
+    context 'コメント投稿できないとき' do
+      it 'textが空では登録できない' do
+        @comment.text = ''
+        @comment.valid?
+        expect(@comment.errors.full_messages).to include("Text can't be blank")
+      end
+    end
+  end
 end
