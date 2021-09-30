c7048f303567bad315d05a19a4d4d4563cc26f9c
atsushikitamura <ax2kx8@gmail.com>
2021-09-30 13:06:01 +0900

    コメント削除機能

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,6 +4,12 @@ class CommentsController < ApplicationController
     redirect_to "/articles/#{comment.article.id}"
   end
 
+  def destroy
+    comment = Comment.find(params[:id])
+    comment.destroy
+    redirect_to "/articles/#{comment.article.id}"
+  end
+
   private
   def comment_params
     params.require(:comment).permit(:text).merge(user_id: current_user.id, article_id: params[:article_id])
