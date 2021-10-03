18eaac611ee434bd73fca31ffaa28af6b8371202
atsushikitamura <ax2kx8@gmail.com>
2021-10-03 22:19:17 +0900

    エラーメッセージ日本語化

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,3 @@
 User.create(name:"sam", email:"sam@sam", password:"samsam1", password_confirmation:"samsam1", admin: true)
 User.create(name:"pra", email:"pra@pra", password:"prapra1", password_confirmation:"prapra1", admin: true)
+User.create(name:"mike", email:"mike@mike", password:"mikemike1", password_confirmation:"mikemike1", admin: true)
