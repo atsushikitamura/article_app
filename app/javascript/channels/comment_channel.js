5c26178bfebadf726f672f1efbbde8316091bc86
atsushikitamura <ax2kx8@gmail.com>
2021-10-03 20:23:00 +0900

    new / edit / show ページの css

--- app/javascript/channels/comment_channel.js
+++ app/javascript/channels/comment_channel.js
@@ -12,13 +12,13 @@ consumer.subscriptions.create("CommentChannel", {
   received(data) {
     const html = `<div id="comment-${data.content.id}">
                     <strong>${data.name}：</strong> ${data.content.text}
-                    <div>
+                    <div class="self-btn">
                       <a data-remote="true" href="${data.content.article_id}/comments/${data.content.id}/edit">編集</a>
                       <a data-remote="true" rel="nofollow" data-method="delete" href="${data.content.article_id}/comments/${data.content.id}">削除</a>
                     </div>
                   </div>`;
     const comments = document.getElementById('comments');
-    const newMessage = document.getElementById('comment_text');
+    const newMessage = document.getElementById('form-text');
     comments.insertAdjacentHTML('afterbegin', html);
     newMessage.value='';
   }
