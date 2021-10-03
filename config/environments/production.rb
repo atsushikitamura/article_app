09ed7a269472c0d008992c4e1cf466ed1e35de49
atsushikitamura <ax2kx8@gmail.com>
2021-10-04 02:25:11 +0900

    ActionCable 追加

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,7 @@
 Rails.application.configure do
+  ActionCable.server.config.disable_request_forgery_protection = true
+  config.action_cable.url = "wss://article-app-35862.herokuapp.com/cable" 
+  config.action_cable.allowed_request_origins = ['https://article-app-35862.herokuapp.com', 'http://article-app-35862.herokuapp.com']
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
